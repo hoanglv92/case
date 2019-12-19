a9c9abd5d53cb2145d3cb12dfcd00f1177bdbb7a
hoanglv92 <hoanglv92@gmail.com>
2019-12-19 14:21:29 +0700

    update

--- ransanmoi.js
+++ ransanmoi.js
@@ -76,11 +76,10 @@ function loop() {
 
 
     // tốc độ rắn trong  game 4fps   //loop to 15 fps instead of 60 - 60/15 = 4
+    // fps cao thì tốc độ cao và ngược lại
 if (point<5) {
     if (++count < 10) {
-
         return;
-
     }
 }else if (point<10){
     if (++count<8){
@@ -106,7 +105,7 @@ if (point<5) {
     context.clearRect(0,0,canvas.width,canvas.height);
 
 
-    snake.x += snake.dx; // mỗi loop rắn sẽ di chuyển thêm 1dx đơn vị
+    snake.x += snake.dx; // mỗi loop rắn sẽ di chuyển thêm 1dx đơn vị, mặc định khi chạy di chuyển sang phải
 
     snake.y += snake.dy;
 
@@ -135,12 +134,12 @@ if (point<5) {
     }
 
 
-    // Phương thức unshift sẽ thêm một hoặc nhiều phần tử vào đầu mảng
+    // Phương thức unshift sẽ thêm một hoặc nhiều phần tử vào đầu mảng khi di chuyển
 
     snake.cells.unshift({x: snake.x, y: snake.y});
 
 
-    // thêm 1 ô vuông phía trc thì phải remove 1 cái phía sau để snake move dc.
+    //tiến lên trước, thêm một phần tử vào mảng xóa 1 phần tử cuối mảng đi
 
     if (snake.cells.length > snake.maxCells) {
 
@@ -149,14 +148,14 @@ if (point<5) {
     }
 
 
-    // draw apple
+    // vẽ con mồi để rắn ăn  'apple'
 
     context.fillStyle = 'red';
 
     context.fillRect(apple.x, apple.y, move-1, move-1);
 
 
-    // draw snake
+    //  mỗi lần cập nhật lại loop, duyệt từng phần tủ của mảng để vẽ lại toàn bộ rắn
 
     context.fillStyle = 'green';
 
@@ -166,9 +165,10 @@ if (point<5) {
         context.fillRect(cell.x, cell.y, move-1, move-1);
 
 
-        // snake chạm vào apple
-
-
+        //  hành động khi snake chạm vào con mồi apple
+        //khi vị trí đầu rắn theo hướng x,y trùng với vị trí của mồi là apple điểm tăng lên một và thay đổi tại ô điểm
+        // mảng snacke  cộng thêm 1 phần tử
+        // và vẽ lại vị trí của con mồi theo random tùy chọn
         if (cell.x === apple.x && cell.y === apple.y) {
 
             point+=1;
@@ -184,14 +184,11 @@ if (point<5) {
         }
 
 
-        // check va chạm khi rắn đụng đuôi
+        // check va chạm khi rắn đụng đuôi,duyệt toàn bộ phần tử  trong mảng
 
         for (var i = index + 1; i < snake.cells.length; i++) {
-
-
-
-            // va chạm thì reset game
-
+            //nếu tại vị trí đầu rắn là cell.x và cell.y trùng với bất kì vị trí nào của từng phần tử trong mảng snack.cells
+            //thì tiến hành reset lại game
             if (cell.x === snake.cells[i].x && cell.y === snake.cells[i].y) {
                 reset();
             }
@@ -202,12 +199,15 @@ if (point<5) {
 
 }
 
-//bắt sự kiện bàn phím ấn xuống
+//bắt sự kiện bàn phím ấn xuống theo mã ASCII
 
 document.addEventListener('keydown', function(e) {
 
     // lọc sự kiện keydown để rắn không di ngược lại
-    //sang trái
+    //sang trái khi phím sang trái được nhấn và snacke.dx=0 thì tại đầu rắn di chuyển sang trái với giá trị là
+    // move=16 trục y giữ nguyên không thay đổi. Cũng như vậy khi nhấn phím lên trên hoặc xuống dưới thì chỉ thay
+    // giá trị theo hướng y hướng x không thay đổi
+
 
     if (e.which === 37 && snake.dx === 0) {
 
